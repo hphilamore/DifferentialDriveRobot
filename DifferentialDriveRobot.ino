9314cbf99b1ff7f4f61a2640016614b5f06d0b3c
hphilamore <hemma.philamore@bristol.ac.uk>
2017-06-18 15:39:34 +0900

    Drive function added taking motor speeds as inputs

--- DifferentialDriveRobot.ino
+++ DifferentialDriveRobot.ino
@@ -1,3 +1,10 @@
+//********************************************************************
+//Differential steering with continuous rotation servos and Arduino Uno
+//Version: 1.0
+//Date: January 2014
+//Author: Stan
+//Web-Site: http://42bots.com/tutorials/differential-steering-with-continuous-rotation-servos-and-arduino/
+//********************************************************************
 #include <Servo.h>
 
 Servo LeftServo;
@@ -15,21 +22,32 @@ void setup()
    RightServo.attach(rightServoPin);
 }
 
-int leftServoSpeed = 90;
-int rightServoSpeed = 0;
-
 void loop() {
-//  
-//  if (leftServoSpeed < 0) {leftServoSpeed = 0;}     
-//  else if (leftServoSpeed > 180) {leftServoSpeed = 180;}
-//
-//  if (rightServoSpeed < 0) {rightServoSpeed = 0;}     
-//  else if (rightServoSpeed > 180) {rightServoSpeed = 180;}
+  //Drive forward for 3 seconds
+   Drive(110, 70);
+   delay(1000);
+   
+   Drive(70, 110);
+   delay(1000);
+   
+   Drive(90, 90);
+   delay(1000);
+}
+
+
+
+void Drive(int leftServoSpeed, int rightServoSpeed) {
+
+  if (leftServoSpeed < 0) {leftServoSpeed = 0;}     
+  else if (leftServoSpeed > 180) {leftServoSpeed = 180;}
+
+  if (rightServoSpeed < 0) {rightServoSpeed = 0;}     
+  else if (rightServoSpeed > 180) {rightServoSpeed = 180;}
 
   //Send the command to the servos
   LeftServo.write(leftServoSpeed);
   RightServo.write(rightServoSpeed);
-  delay(1000);
+  
 }
 
 
